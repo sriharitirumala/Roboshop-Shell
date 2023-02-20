7b6dfcf3aac099c106bf9319a8ef0db96bb371dd
sriharitirumala <sritirumala30@gmail.com>
2023-02-20 20:53:35 +0530

    [Token-4] Roboshop components

--- mongodb.sh
+++ mongodb.sh
@@ -3,5 +3,5 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 
-#update /etc/momgod.conf file from 127.0.0.1 with 0.0.0.0
+#update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
 
