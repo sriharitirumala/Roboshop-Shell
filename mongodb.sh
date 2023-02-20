3b9f93d5ebf358a469dc1aadb4e44365ccb5ea02
sriharitirumala <sritirumala30@gmail.com>
2023-02-20 20:51:52 +0530

    [Token-4] Roboshop components

---
+++ mongodb.sh
@@ -0,0 +1,7 @@
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+
+#update /etc/momgod.conf file from 127.0.0.1 with 0.0.0.0
+
