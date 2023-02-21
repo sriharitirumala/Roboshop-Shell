4d23f67246def7a3705ca4d4173d9de1494688ec
sriharitirumala <sritirumala30@gmail.com>
2023-02-21 14:48:42 +0530

    [Token-6] variable

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,18 @@
 code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
 
-yum install nginx -y
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+yum install nginx -y &>>${log_file}
+
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log_file}
 
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
-systemctl restart nginx
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+
+systemctl enable nginx &>>${log_file}
+systemctl restart nginx &>>${log_file}
 
