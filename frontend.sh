fe9cc146efae178212597555d26121af122c70f8
sriharitirumala <sritirumala30@gmail.com>
2023-02-21 14:10:27 +0530

    [Token-6] variable

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 systemctl restart nginx
 
