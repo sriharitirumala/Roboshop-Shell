24a1fa25c289580c6849c184b83abe17c5506fc9
sriharitirumala <sritirumala30@gmail.com>
2023-02-21 17:19:29 +0530

    [Token-6] added new items

--- catalogue.sh
+++ catalogue.sh
@@ -1,22 +1,46 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+print_head  "configure nodejs repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+print_head "Install nodejs"
+yum install nodejs -y &>>${log_file}
+
+print_head "Create User roboshop"
+useradd roboshop &>>${log_file}
+
+print_head "Create Application Directory to roboshop"
+mkdir /app &>>${log_file}
+
+print_head "Delete old content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Download the app content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp configs/catalogue.service etc/systemd/system/catalogue.service
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+print_head "extracting content"
+unzip /tmp/catalogue.zip &>>${log_file}
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+print-head "installing nodejs"
+npm install &>>${log_file}
 
-mongo --host mongodb.devopsb71service.site </app/schema/catalogue.js
+print-head "copy systemD service files"
+cp configs/catalogue.service etc/systemd/system/catalogue.service &>>${log_file}
+
+print-head "reload catalogue"
+systemctl daemon-reload &>>${log_file}
+
+print-head "enabling catalogue"
+systemctl enable catalogue &>>${log_file}
+
+print_head "restarting catalogue"
+systemctl restart catalogue &>>${log_file}
+
+print_head "copying mongodb repo file"
+cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "installing mongodb"
+yum install mongodb-org-shell -y &>>${log_file}
+
+mongo --host mongodb.devopsb71service.site </app/schema/catalogue.js &>>${log_file}
