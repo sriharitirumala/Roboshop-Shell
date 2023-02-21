5336a9f149e2c5899567919a4967110e936b4031
sriharitirumala <sritirumala30@gmail.com>
2023-02-21 15:45:57 +0530

    [Token-6] added new items

--- frontend.sh
+++ frontend.sh
@@ -9,24 +9,24 @@ print_head() {
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
 
-print_head "Removing nginx"
+print_head "Removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 
-print_head "Downloading nginx"
+print_head "Downloading frontend"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
-print_head "Unzipping nginx"
+print_head "Unzipping nginx for roboshop"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 
-print_head "Copying nginx"
+print_head "Copying nginx configs"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
 
-print_head "Restarting nginx"
-systemctl restart nginx &>>${log_file}
+print_head "starting nginx"
+systemctl start nginx &>>${log_file}
 
 
 }
