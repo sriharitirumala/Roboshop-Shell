d71f74b3e4bf1acb5abd3bc26c74e55effe4ce3a
sriharitirumala <sritirumala30@gmail.com>
2023-02-21 16:11:29 +0530

    [Token-6] added new items

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,6 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
+source common.sh
 
-print_head() {
-  echo -e "\e[32m$1\e[0m"
-  }
-  
+
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
 
--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,15 @@
+source common.sh
+
+print_head "Setup mongodb repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install mongodb"
 yum install mongodb-org -y
+
+print_head "Enable mongodb"
 systemctl enable mongod
+
+print_head "Start mongodb"
 systemctl start mongod
 
 #update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
