e6a1e356ca7e0fdfa54829602060a0f5a6ec50b8
sriharitirumala <sritirumala30@gmail.com>
2023-02-22 16:08:29 +0530

    [token-8] Adding status_checks

--- common.sh
+++ common.sh
@@ -18,3 +18,70 @@ status_check() {
   fi
 }
 
+NODEJS(){
+  print_head  "Configure NodeJS Repo"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
+
+  print_head "Install NodeJS"
+  yum install nodejs -y &>>${log_file}
+  status_check $?
+
+  print_head "Create User roboshop"
+  id roboshop &>>${log_file}
+  if [ $? -ne 0 ]; then
+   useradd roboshop &>>${log_file}
+  fi
+  status_check $?
+
+  print_head "Create Application Directory to roboshop"
+  if [ ! -d /app ]; then
+   mkdir /app &>>${log_file}
+  fi
+  status_check $?
+
+  print_head "Delete old content"
+  rm -rf /app/* &>>${log_file}
+  status_check $?
+
+  print_head "Download the app content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+  cd /app
+  status_check $?
+
+  print_head "Extracting content"
+  unzip /tmp/${component}.zip &>>${log_file}
+  status_check $?
+
+  print_head "Installing NodeJS dependencies"
+  npm install &>>${log_file}
+  status_check $?
+
+  print_head "Copy systemD service files"
+  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  status_check $?
+
+  print_head "reload ${component}"
+  systemctl daemon-reload &>>${log_file}
+  status_check $?
+
+  print_head "enabling ${component}"
+  systemctl enable ${component} &>>${log_file}
+  status_check $?
+
+  print_head "restarting ${component}"
+  systemctl restart ${component} &>>${log_file}
+  status_check $?
+
+  print_head "copying mongodb repo file"
+  cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+  status_check $?
+
+  print_head "installing mongodb client"
+  yum install mongodb-org-shell -y &>>${log_file}
+  status_check $?
+
+  print_head "load schema"
+  mongo --host mongodb.devopsb71services.site </app/schema/${component}.js &>>${log_file}
+  status_check $?
+}
