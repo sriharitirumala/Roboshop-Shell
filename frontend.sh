70bc5e88269bca9519a1c5315b219b61bccc426d
sriharitirumala <sritirumala30@gmail.com>
2023-02-22 13:47:16 +0530

    [Token-7] added new items

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,11 @@ source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 print_head "Removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
