7d434f5ab1bda5db9490024a2278a20adad91775
sriharitirumala <sritirumala30@gmail.com>
2023-02-22 10:55:11 +0530

    [Token-6] added new items

--- mongodb.sh
+++ mongodb.sh
@@ -1,17 +1,17 @@
 source common.sh
 
-print_head "Setup mongodb repository"
+print_head "Setup MongoDB repository"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
-print_head "Install mongodb"
+print_head "Install MongoDB"
 yum install mongodb-org -y &>>${log_file}
 
-print_head "Update mongodb listen address"
+print_head "Update MongoDB listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>${log_file}
 
-print_head "Enable mongodb"
+print_head "Enable MongoDB"
 systemctl enable mongod &>>${log_file}
 
-print_head "Start mongodb"
+print_head "Start MongoDB"
 systemctl restart mongod &>>${log_file}
 
