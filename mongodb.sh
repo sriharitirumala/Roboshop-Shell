5157d46a012287c79fa594f2435becb78a920293
sriharitirumala <sritirumala30@gmail.com>
2023-02-22 11:01:32 +0530

    [Token-6] added new items

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ print_head "Install MongoDB"
 yum install mongodb-org -y &>>${log_file}
 
 print_head "Update MongoDB listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>${log_file}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 
 print_head "Enable MongoDB"
 systemctl enable mongod &>>${log_file}
