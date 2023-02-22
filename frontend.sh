2e73b96707f22ca6e0bc973f3da71f30f4570ec9
sriharitirumala <sritirumala30@gmail.com>
2023-02-22 13:52:49 +0530

    [Token-7] added new items

--- frontend.sh
+++ frontend.sh
@@ -11,25 +11,54 @@ fi
 
 print_head "Removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 print_head "Downloading frontend"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 
 print_head "Unzipping nginx for roboshop"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 
 print_head "Copying nginx configs"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 
 print_head "starting nginx"
 systemctl restart nginx &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
