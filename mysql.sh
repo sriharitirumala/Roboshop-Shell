146821781dab42b40da95f3fa5967557321b56c5
sriharitirumala <sritirumala30@gmail.com>
2023-02-22 17:10:12 +0530

    [token-8] Adding status_checks

--- mysql.sh
+++ mysql.sh
@@ -9,23 +9,23 @@ fi
 mysql
 
 print_head "Disabling Mysql 8 version"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>${log_file}
 status_check $?
 
 print_head "Installing mysql server"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>${log_file}
 status_check $?
 
 print_head "Enabling mysql server"
-systemctl enable mysqld
+systemctl enable mysqld &>>${log_file}
 status_check $?
 
 print_head "Starting mysql server"
-systemctl start mysqld
+systemctl start mysqld &>>${log_file}
 status_check $?
 
 print_head "Set password"
-mysql_secure_installation --set-root-pass ${mysql_root_password}
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
 status_check $?
 
 
