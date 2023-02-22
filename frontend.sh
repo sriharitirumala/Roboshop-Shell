fc0999291849838c432f1fdb392cf293f0df5fd5
sriharitirumala <sritirumala30@gmail.com>
2023-02-22 14:00:24 +0530

    [Token-7] added new items

--- frontend.sh
+++ frontend.sh
@@ -3,62 +3,29 @@ source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check $?
 
 print_head "Removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check $?
 
 print_head "Downloading frontend"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
-
+status_check $?
 
 print_head "Unzipping nginx for roboshop"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
-
+status_check $?
 
 print_head "Copying nginx configs"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
-
+status_check $?
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
-
+status_check $?
 
 print_head "starting nginx"
 systemctl restart nginx &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
-
+status_check $?
