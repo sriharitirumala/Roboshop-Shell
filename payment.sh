3b74996f0397549d469f25ce00e8bb290428daf8
sriharitirumala <sritirumala30@gmail.com>
2023-02-23 16:52:42 +0530

    [token-9] Adding shipping.service

--- payment.sh
+++ payment.sh
@@ -2,7 +2,7 @@ source common.sh
 
 roboshop_app_password=$1
 if [ -z "${roboshop_app_password}" ]; then
-  echo -e "\e[31mrabbitmq app user password\e[0m"
+  echo -e "\e[31mMissing RabbitMQ App User Password argument\e[0m"
   exit 1
 fi
 
